3f81f9f2080d71410db4ea7b00366dfa0060449c
denmcca <denmcca@gmail.com>
2017-09-09 11:13:47 -0700

    Update Gui.java

--- Gui.java
+++ Gui.java
@@ -1,9 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package barcodescanner;
+
 import java.awt.Font;
 import java.awt.GridLayout;
 import javax.swing.JFrame;
